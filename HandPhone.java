14ac4fe5a410ef3bc60c563e6c247ed4c3764977
nadilajm <jannatulnadila@gmail.com>
2024-03-01 23:39:48 +0700

    sesi 2

---
+++ HandPhone.java
@@ -0,0 +1,28 @@
+import java.io.OutputStream;
+
+public class HandPhone {
+    String jenis_hp;
+    int tahun_pembuatan;
+
+    public void setDataHP(String jenis_hp, int tahun_pembuatan){
+        this.jenis_hp = jenis_hp;
+        this.tahun_pembuatan = tahun_pembuatan;
+    }
+
+    String getJenisHP(){
+        return jenis_hp;
+    }
+    int getTahunPembuatan(){
+        return tahun_pembuatan;
+    }
+
+    public static void main(String[] args) {
+        HandPhone hp = new HandPhone();
+        hp.setDataHP("ios", 2021);
+        hp.getJenisHP();
+        hp.getTahunPembuatan();
+    }
+
+    
+}
+
